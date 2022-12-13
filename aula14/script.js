ee79662ea61a5f7b719f2229ce661b2cd785f337
Jefferson Eneas da Silva <jeffersoneneassilva@gmail.com>
2022-12-13 20:59:02 -0300

    projeto idade

--- aula14/script.js
+++ aula14/script.js
@@ -1,17 +1,49 @@
-function carregar() {
-    var msg = document.getElementById('msg');
-    var img = document.getElementById('imagem');
+function verificar() {
     var data = new Date();
-    var hora = data.getHours();
-    msg.innerHTML = `Agora são ${hora} horas`
-    if (hora >= 0 && hora <12) {
-        img.src = 'img/manhã.jpg'
-        document.body.style.background = '#EFB27D'
-    } else if (hora >= 12 && hora <18) {
-        img.src = 'img/tarde.jpg'
-        document.body.style.background = '#BA631E'
+    var ano = data.getFullYear();
+    var fano = document.getElementById('txtano')
+    var res = document.querySelector('div#res')
+    if (fano.value.length == 0 || Number(fano.value) > ano) {
+        window.alert('[ERRO]Verifique os dados e tente novamente!')
     } else {
-        img.src = 'img/noite.jpg'
-        document.body.style.background = '#57466E'
+        var fsex = document.getElementsByName('radsex')
+        var idade = ano - Number(fano.value)
+        var genero = ''
+        var img = document.createElement('img')
+        img.setAttribute('id', 'foto')
+        if (fsex[0].checked) {
+            genero = 'Homem'
+            if (idade >= 0 && idade < 10) {
+                // Criança
+                img.setAttribute('src', 'img/bebe-m.jpg')
+            } else if (idade < 21) {
+                // Jovem
+                img.setAttribute('src', 'img/jovem-m.jpg')
+            } else if (idade < 50) {
+                // Adulto
+                img.setAttribute('src', 'img/adulto-m.jpg')
+            } else {
+                // Idoso
+                img.setAttribute('src', 'img/idoso-m.jpg')
+            }
+        } else if (fsex[1].checked) {
+            genero = 'Mulher'
+            if (idade >= 0 && idade < 10) {
+                // Criança
+                img.setAttribute('src', 'img/bebe-f.jpg')
+            } else if (idade < 21) {
+                // Jovem
+                img.setAttribute('src', 'img/jovem-f.jpg')
+            } else if (idade < 50) {
+                // Adulto
+                img.setAttribute('src', 'img/adulto-f.jpg')
+            } else {
+                // Idoso
+                img.setAttribute('src', 'img/idoso-f.jpg')
+            }
+        }
+        res.innerHTML = `Detectamos ${genero} com ${idade} anos.`
+        res.appendChild(img)
     }
 }
+
